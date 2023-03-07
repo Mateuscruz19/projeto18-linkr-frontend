6224be3f042713e1fdea3a7474a8217a1c918385
Leonardo Satoru Osugi <leoosugi96@gmail.com>
2023-03-07 04:33:44 -0300

    feat: Tela Estatica dos Posts

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Component } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./components/Login.js"
 import Register from "./components/Register.js"
+import Posts from "./pages/PostPages/PostPages.js";
 
 export default function App() {
 
@@ -13,6 +14,7 @@ export default function App() {
           <Route path="/" element={<Component/>} />
           <Route path="/login" element={<Login/>} />
           <Route path="/registro" element={<Register/>} />
+          <Route path="/post" element={<Posts/>}/>
         </Routes>
       </BrowserRouter>
   );
---
+++ src/components/ConteinerPost.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const ConteinerPost = styled.div`
+        width: 100%;
+        height: 100%;
+        background-color: #333333;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+`;
