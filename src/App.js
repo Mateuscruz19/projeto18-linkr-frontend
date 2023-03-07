f822a14ad9cce68ac8a66c76dbf96a9eafb3d446
Mateuscruz19 <profissionalmateuscruz@gmail.com>
2023-03-06 21:25:23 -0300

    Feature:Login Layout

--- src/App.js
+++ src/App.js
@@ -1,28 +1,20 @@
 import { Component } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import styled from "styled-components";
+import Login from "./components/Login.js"
+import Register from "./components/Register.js"
 
 export default function App() {
 
     
 
   return (
-    <ContainerApp>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Component/>} />
-          <Route path="/login" element={<Component/>} />
-          <Route path="/cadastro" element={<Component/>} />
+          <Route path="/login" element={<Login/>} />
+          <Route path="/cadastro" element={<Register/>} />
         </Routes>
       </BrowserRouter>
-    </ContainerApp>
   );
 }
 
-const ContainerApp = styled.div`
-
-  height: 100%;
-  min-height: 100vh;
-  background-color: black; 
-
-`;
