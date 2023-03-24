2e32d7f4cd265cb2ee581343c5ae6421788f2261
apocsenpai <jonatassennas@ufrrj.br>
2023-03-24 19:59:47 -0300

    fix: show correct author posts in comments

--- src/components/Post/CommentCard/CommentCard.js
+++ src/components/Post/CommentCard/CommentCard.js
@@ -1,13 +1,14 @@
+import { Link } from "react-router-dom";
 import { CommentContent, CommentImage } from "../Comments/styled";
 
-const CommentCard = ({name, avatarImage, comment, status }) => {
+const CommentCard = ({name,userId, avatarImage, comment, status }) => {
   return (
     <>
       <CommentContent  data-test="comment">
         <CommentImage src={avatarImage} alt={`Youuu`} />
         <div>
           <p>
-            <span>{name}</span> <span>{status && `• ${status}`}</span>
+            <span><Link to={`/user/${userId}`}>{name}</Link></span> <span>{status && `• ${status}`}</span>
           </p>
           <p>{comment}</p>
         </div>
